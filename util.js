e8d6eb72a28af24817a21d6c63f67f1830b874cb
Samardeep Singh Anand <singhsamardeep9@gmail.com>
2022-08-29 09:10:03 +0530

    utility file

---
+++ util.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "Documents" : ["doc" , "pdf", "txt"],
+    "Audio" : ["mp3"],
+    "Video" : ["mp4" , "mkv"],
+    "Image" : ["jpg" , "png" , "img" , "gif"],
+    "Applications" : ["exe"]
+}
